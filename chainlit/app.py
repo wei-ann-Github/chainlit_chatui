0527fc73e34594ed9ddaa97b097901b6c6dfc327
wei-a@surfaceLaptop <wei-ann.lim@outlook.com>
2023-08-26 09:17:53 +0800

    in memory cache

--- chainlit/app.py
+++ chainlit/app.py
@@ -1,18 +1,19 @@
 import os
 
-from datetime import datetime
-from langchain import PromptTemplate, OpenAI, LLMChain
+from langchain import LLMChain
 import chainlit as cl
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
 
 
 system_message = ""
-messages = [
+init_messages = [
     SystemMessage(
         content=system_message
     ),
 ]
+
+cache = dict()
 
 @cl.on_chat_start
 def main():
@@ -21,30 +22,37 @@ def main():
                       openai_api_key=os.environ.get('OPENAI_API_KEY'),
                       model='gpt-3.5-turbo'
                      )
-    
-    # Session ID
-    session_id = datetime.now().strftime('%H%M%S%f')
 
     # Store the chain in the user session
-    cl.user_session.set('session_id', session_id)
     cl.user_session.set("chat", chat)
 
 
 @cl.on_message
 async def main(message: str):
-    # Retrieve the chain from the user session
+    # Retrieve chain and session id from user_session
     llm_chain = cl.user_session.get("chat")  # type: LLMChain
+    session_id = cl.user_session.get("id")
+    
+    # Initialize the cache
+    messages = cache.get(session_id, init_messages)
 
     human_message = HumanMessage(content=message)
     messages.append(human_message)
 
+    # update cache everytime messages get updated.
+    cache[session_id] = messages
+
     # Call the chain asynchronously
-    res = llm_chain(messages, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    res = llm_chain(messages, callbacks=[cl.AsyncLangchainCallbackHandler()])  # comment for testing.
+    # res = AIMessage(content='hello, this is a dummy reply. I am not hooked up to a LLM yet.')  # uncomment for testing.
     messages.append(res)
+
+    # update cache everytime messages get updated.
+    cache[session_id] = messages
 
     # Do any post processing here
     # TODO - Save chat to DB.
-    session_id = cl.user_session.get("session_id")
 
     # Send the response
-    await cl.Message(content=res.content).send()
+    await cl.Message(content=res.content).send()  # comment for testing.
+    # await cl.Message(content=res).send()  # uncomment for testing.
